cd7dc601169cd8d351b644e4e4204f953b9eff5c
ToCraft <piminecraft492@gmail.com>
2024-05-03 13:07:19 +0200

    add DataSaver for SkillList

--- common/src/main/java/tocraft/wwdatagen/config/DataSaver.java
+++ common/src/main/java/tocraft/wwdatagen/config/DataSaver.java
@@ -6,6 +6,7 @@ import com.mojang.datafixers.util.Either;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.JsonOps;
 import dev.architectury.platform.Platform;
+import tocraft.walkers.api.data.skills.SkillDataManager;
 import tocraft.walkers.api.data.variants.TypeProviderDataManager;
 
 import java.io.IOException;
@@ -15,18 +16,36 @@ import java.nio.file.Paths;
 
 public class DataSaver {
     private static final Path GENERATED_PATH = Paths.get(Platform.getConfigFolder().toString(), "walkers/generated");
-
+    private static final Path VARIANTS_PATH = Paths.get(GENERATED_PATH.toString(), "data/auto/alkers/variants");
+    private static final Path SKILLS_PATH = Paths.get(GENERATED_PATH.toString(), "data/auto/alkers/skills");
 
     public static void save(TypeProviderDataManager.TypeProviderEntry<?> typeProviderEntry) {
         JsonElement json = TypeProviderDataManager.TYPE_PROVIDER_LIST_CODEC.encodeStart(JsonOps.INSTANCE, Either.left(typeProviderEntry)).get().orThrow();
-        write(Paths.get(GENERATED_PATH.toString(), typeProviderEntry.entityTypeKey().getNamespace() + "_" + typeProviderEntry.entityTypeKey().getPath() + ".json"), json);
+        write(Paths.get(VARIANTS_PATH.toString(), typeProviderEntry.entityTypeKey().getNamespace() + "_" + typeProviderEntry.entityTypeKey().getPath() + ".json"), json);
+    }
+
+    public static void save(SkillDataManager.SkillList skillList) {
+        JsonElement json = SkillDataManager.SKILL_LIST_CODEC.encodeStart(JsonOps.INSTANCE, skillList).get().orThrow();
+        String fileName = skillList.requiredMod();
+        if (!skillList.entityTypeKeys().isEmpty()) fileName += "_" + skillList.entityTypeKeys().get(0).getPath();
+        if (!skillList.entityTagKeys().isEmpty()) fileName += "_" + skillList.entityTagKeys().get(0).getPath();
+        write(Paths.get(SKILLS_PATH.toString(), fileName + ".json"), json);
+    }
+
+    private static void createDirectories() throws IOException {
+        if (Files.exists(GENERATED_PATH))
+            Files.createDirectories(GENERATED_PATH);
+
+        if (Files.exists(VARIANTS_PATH))
+            Files.createDirectories(VARIANTS_PATH);
+
+        if (Files.exists(SKILLS_PATH))
+            Files.createDirectories(SKILLS_PATH);
     }
 
     private static void write(Path file, JsonElement json) {
         try {
-            if (!Files.exists(GENERATED_PATH)) {
-                Files.createDirectories(GENERATED_PATH);
-            }
+            createDirectories();
 
             if (!Files.exists(file)) {
                 Files.createFile(file);
